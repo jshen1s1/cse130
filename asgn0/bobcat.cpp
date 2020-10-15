1d42bf9011309c5b514ac3bfeab39b9e2e5b758a
jshen30 <jshen30@ucsc.edu>
2020-10-14 23:44:05 -0700

    Fixed error response

--- asgn0/bobcat.cpp
+++ asgn0/bobcat.cpp
@@ -10,35 +10,32 @@ int main(int argc, char* argv[]){
     char in[8192]; //use to store the char read from stdin 
 
     if(argc == 1){ //if no file specified in command
-        while(read(0, in, 1) > 0){
+        while(read(0, in, 1) > 0){ //read from standard input
                 write(1, in, 1);
         }
     }
     else{
         for (int i=1; i< argc; i++){ //go through all files specified in command
-            if(!strcmp(argv[i], "-")){
+            if(!strcmp(argv[i], "-")){ //if '-' as input
                 while(read(0, in, 1) > 0){
                     write(1, in, 1);
                 }
    
             }
             else{
-                fd = open(argv[i], O_RDONLY);
+                fd = open(argv[i], O_RDONLY); 
 
                 if(fd < 0){
-                    perror("Bobcat: ");
+                    perror(argv[i]); //gives an error message when cannot access file
                     exit(1);
                 }else{
-                    while(read(fd, in, 1)>0){
+                    while(read(fd, in, 1)>0){ //read from the file
                         write(1, in, 1);
                     }
                     close(fd);
                 }
-
             }
         }
     }
-
     return (EXIT_SUCCESS);
-    
 }
