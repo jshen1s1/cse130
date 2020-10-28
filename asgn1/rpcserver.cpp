3801fe6c786a1c858e8b5f555ad130fae374169d
jshen30 <jshen30@ucsc.edu>
2020-10-27 17:35:10 -0700

    Update the design pdf, add math  function design

--- asgn1/rpcserver.cpp
+++ asgn1/rpcserver.cpp
@@ -11,10 +11,10 @@
 
 
 int main(int argc, char* argv[]){
-    struct hostent *hent = gethostbyname(SERVER_NAME_STRING /* eg "localhost" */); //setup sockets
+    struct hostent *hent = gethostbyname("localhost" /* eg "localhost" */); //setup sockets
     struct sockaddr_in addr;
     memcpy(&addr.sin_addr.s_addr, hent->h_addr, hent->h_length);
-    addr.sin_port = htons(PORT_NUMBER);
+    addr.sin_port = htons(atoi(argv[1]));
     addr.sin_family = AF_INET;
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,13 +25,51 @@ int main(int argc, char* argv[]){
     listen(sock, 0);
     int cl = accept(sock, NULL, NULL);
 
-    connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+    uint8_t recvBuf[16384];
+    uint16_t function;
+    uint32_t identifier;
+    size_t recvSize;
+    do{
+        recvSize = read(cl, &recvBuf, 16384);
+        if(recvSize > 0){
+            for(size_t i=0; i<recvSize; i++){
+                printf("%02x", recvBuf[i]);
+            }
+            printf("\nBytes recvied: %d\n", recvSize);
 
+            function = (uint16_t)recvBuf[0] << 8 | recvBuf[1];
+            printf("\nFunction called: %04x\n", function);
 
-    uint8_t type8;
-    uint16_t type16;
-    uint32_t type32;
-    uint64_t type64;
-    uint8_t *words;
+            for(int j=2; j<6; j++){
+                identifier = identifier << 8 | recvBuf[j];
+            }
+            printf("\nidentifier: %08x\n", identifier);
+
+            switch(function){
+                case 0x0101 :
+                    math(recvBuf);
+                    printf("\nAdd function called: %04x\n", function);
+                    break;
+                case 0x0102 :
+                    printf("\nSub function called: %04x\n", function);
+                    break;
+                case 0x0103 :
+                    printf("\nMul function called: %04x\n", function);
+                    break;
+            }
+        }
+        else if(recvSize==0){
+            printf("Connection closed\n");
+        }
+        else{
+            printf("Recv failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }while(recvSize > 0);
+
+    return(EXIT_SUCCESS);
+}
+
+int64_t math(uint8_t buffer[]){
 
 }
